34ff0bcca43b9993aa2925b0e5cf8500ec637cc4
Vaflor Jonathan <VaflorJonathan@JohnDeere.com>
2020-07-29 11:08:12 -0500

    created final game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,28 +22,53 @@ var cards = [
 ]
 
 var cardsInPlay = [];
+var points = 0;
 
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2){
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  	console.log("You found a match!");
+  	alert("You found a match!");
+  	points += 1;
+  	document.getElementById("points").textContent = points;
 } 	else {
   	alert("Sorry, try again.");
-}
-}
-}
+};
+};
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+   var cardId = this.getAttribute('data-id');
+   cardsInPlay.push(cards[cardId].rank);
+   this.setAttribute('src', cards[cardId].cardImage);
+
+   if (cardsInPlay.length === 2){
+   	setTimeout(checkForMatch, 280)
+   };
 
    console.log("User flipped"+" "+ cards[cardId].rank);
-   cardsInPlay.push(cards[cardId].rank);
-   checkForMatch();
    console.log(cards[cardId].cardImage);
    console.log(cards[cardId].suit);
 
-}
-flipCard(0);
-flipCard(2);
+};
 
+var createBoard = function () {
+	for (let i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png")
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
 
+var resetBoard = function () {
+	while (cardsInPlay.length > 0) {
+		cardsInPlay.pop(0);
+	}
+	document.getElementById('game-board').innerHTML = "";
+	createBoard();
+};
 
+createBoard();
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetBoard);
